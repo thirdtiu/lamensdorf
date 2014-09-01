93d1ed3813f8af57e8db147e32489126ec074361
Avelino Tiu III <thirdtiu@gmail.com>
2014-08-31 19:37:13 -0500

    form stack

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(function(){
+
+	// scroll to top stuff
 	$().UItoTop({ easingType: 'easeOutQuart', text: 'Scroll<br>to Top' });
+
+	/* formstack overrides*/
+	$('.fsFormatEmail').attr('placeholder', 'Email Address');
+	$('a[title*="Online"]').hide();
 });
